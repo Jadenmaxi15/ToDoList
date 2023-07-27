7d6888a6590ad247ee745c60487f453f24958ba9
Jadenmaxi15 <68132867+Jadenmaxi15@users.noreply.github.com>
2023-07-27 22:30:54 +0530

    Update to-do-list

--- to-do-list.py
+++ to-do-list.py
@@ -1,76 +1,63 @@
-#importing packages 
-from  tkinter import * 
+from tkinter import *
+from PIL import ImageTk, Image
 import tkinter.messagebox
-#function to enter the task in the Listbox
+
 def entertask():
-    #A new window to pop up to take input
     input_text=""
     def add():
+        nonlocal input_text
         input_text=entry_task.get(1.0, "end-1c")
         if input_text=="":
             tkinter.messagebox.showwarning(title="Warning!",message="Please Enter some Text")
         else:
             listbox_task.insert(END,input_text)
-            #close the root1 window
             root1.destroy()
     root1=Tk()
     root1.title("Add task")
-    entry_task=Text(root1,width=40,height=4)
-    entry_task.pack()
-    button_temp=Button(root1,text="Add task",command=add)
-    button_temp.pack()
+    entry_task=Text(root1,width=40,height=4, font=("Helvetica", 12))
+    entry_task.pack(pady=10)
+    button_temp=Button(root1,text="Add task", font=("Helvetica", 12), bg="green", fg="white", command=add)
+    button_temp.pack(pady=10)
     root1.mainloop()
-    
 
-#function to facilitate the delete task from the Listbox
 def deletetask():
-    #selects the slected item and then deletes it 
     selected=listbox_task.curselection()
     listbox_task.delete(selected[0])
-#Executes this to mark completed 
 
 def markcompleted():
     marked=listbox_task.curselection()
     temp=marked[0]
-    #store the text of selected item in a string
     temp_marked=listbox_task.get(marked)
-    #update it 
     temp_marked=temp_marked+" ✔"
-    #delete it then insert it 
     listbox_task.delete(temp)
     listbox_task.insert(temp,temp_marked)
-#creating the initial window
-window=Tk()
-#giving a title
-window.title("DataFlair To_Do_APP")
 
+window = Tk()
+window.title("To-Do App")
 
-#Frame widget to hold the listbox and the scrollbar
-frame_task=Frame(window)
-frame_task.pack()
+# Load and display the background image
+background_image = ImageTk.PhotoImage(Image.open("background.jpg"))
+background_label = Label(window, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-#to hold items in a listbox
-listbox_task=Listbox(frame_task,bg="black",fg="white",height=15,width=50,font = "Helvetica")  
-listbox_task.pack(side=tkinter.LEFT)
+frame_task = Frame(window, bg="white")
+frame_task.place(x=50, y=50)
 
-#Scrolldown in case the total list exceeds the size of the given window 
-scrollbar_task=Scrollbar(frame_task)
-scrollbar_task.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+listbox_task = Listbox(frame_task, bg="black", fg="white", height=15, width=50, font=("Helvetica", 12))  
+listbox_task.grid(row=0, column=0)
+
+scrollbar_task = Scrollbar(frame_task)
+scrollbar_task.grid(row=0, column=1, sticky="NS")
 listbox_task.config(yscrollcommand=scrollbar_task.set)
 scrollbar_task.config(command=listbox_task.yview)
 
+entry_button = Button(window, text="Add task", font=("Helvetica", 12), bg="green", fg="white", width=50, command=entertask)
+entry_button.place(x=50, y=400)
 
-#Button widget 
-entry_button=Button(window,text="Add task",width=50,command=entertask)
-entry_button.pack(pady=3)
+delete_button = Button(window, text="Delete selected task", font=("Helvetica", 12), bg="red", fg="white", width=50, command=deletetask)
+delete_button.place(x=50, y=450)
 
-delete_button=Button(window,text="Delete selected task",width=50,command=deletetask)
-delete_button.pack(pady=3)
-
-mark_button=Button(window,text="Mark as completed ",width=50,command=markcompleted)
-mark_button.pack(pady=3)
-
+mark_button = Button(window, text="Mark as completed", font=("Helvetica", 12), bg="blue", fg="white", width=50, command=markcompleted)
+mark_button.place(x=50, y=500)
 
 window.mainloop()
-
-
